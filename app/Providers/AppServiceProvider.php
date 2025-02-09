066cdf8451aa46ea529ab9227d65de05ce8bccce
bola-cloud <bola.ishak41@gmail.com>
2025-02-09 02:33:41 +0200

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Category;
+use Illuminate\Support\Facades\Schema;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot()
+    {
+        // Check if the 'categories' table exists before querying
+        if (Schema::hasTable('categories')) {
+            $categories = Category::get();
+            View::share('categories', $categories);
+        }
+    }
+}
