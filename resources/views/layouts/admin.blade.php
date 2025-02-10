8ac401e3e8de64f1ccffb45d632c4d32147df43c
bola-cloud <bola.ishak41@gmail.com>
2025-02-10 02:38:15 +0200

    dfd

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,9 +4,9 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <title>Screen Video</title>
+    <title>Foresight</title>
     <link rel="apple-touch-icon" href="{{asset("app-assets/images/ico/apple-icon-120.png")}}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset("app-assets/images/ico/favicon.ico")}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset("theme/assets/images/logo/logo.jpg")}}">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700"
     rel="stylesheet">
     <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css"
@@ -79,7 +79,7 @@
                     <li class="nav-item mobile-menu d-md-none mr-auto"><a class="nav-link nav-menu-main menu-toggle hidden-xs" href="#"><i class="ft-menu font-large-1"></i></a></li>
                     <li class="nav-item">
                         <a class="navbar-brand" href="{{route('dashboard')}}">
-                          <img class="brand-logo" alt="modern admin logo" src="{{asset('theme/assets/images/logo/white tiger.jpeg')}}"
+                          <img class="brand-logo" alt="modern admin logo" src="{{asset('theme/assets/images/logo/logo.jpg')}}"
                           style="height: 55px; width: 178px;">
                           {{-- <h3 class="brand-text">Screen Video </h3> --}}
                         </a>
